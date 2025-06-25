a16cd7f1dda1516c5a521812e32c4f76967deccf
Dinesh <dineshlodhiglt@gmail.com>
2025-06-26 01:31:50 +0530

    app.py

--- app.py
+++ app.py
@@ -4,20 +4,12 @@ from tensorflow.keras.preprocessing import image
 import numpy as np
 from PIL import Image
 
-
-model = load_model(r'D:\pickle files\plant\PlantVillage_CNN_model.h5')
-
-
-
+model = load_model('PlantVillage_CNN_model.h5')
 
 class_names = ['Pepper__bell___Bacterial_spot', 'Pepper__bell___healthy', 'Potato___Early_blight', 'Potato___healthy', 'Potato___Late_blight', 'Tomato_Bacterial_spot', 'Tomato_Early_blight', 'Tomato_healthy', 'Tomato_Late_blight', 'Tomato_Leaf_Mold', 'Tomato_Septoria_leaf_spot', 'Tomato_Spider_mites_Two_spotted_spider_mite', 'Tomato__Target_Spot', 'Tomato__Tomato_mosaic_virus', 'Tomato__Tomato_YellowLeaf__Curl_Virus', 'Tomato_Late_blight']
 
-
-
-
 st.title("🌿 Plant Disease Classifier")
 st.write("Upload a plant leaf image and I'll predict the disease category.")
-
 
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
