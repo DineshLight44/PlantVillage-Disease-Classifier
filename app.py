ce0814e709b1a2ed88b13b4a6f3a4d2aabacbcb0
Dinesh <dineshlodhiglt@gmail.com>
2025-06-26 01:29:11 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
+from PIL import Image
+
+
+model = load_model(r'D:\pickle files\plant\PlantVillage_CNN_model.h5')
+
+
+
+
+class_names = ['Pepper__bell___Bacterial_spot', 'Pepper__bell___healthy', 'Potato___Early_blight', 'Potato___healthy', 'Potato___Late_blight', 'Tomato_Bacterial_spot', 'Tomato_Early_blight', 'Tomato_healthy', 'Tomato_Late_blight', 'Tomato_Leaf_Mold', 'Tomato_Septoria_leaf_spot', 'Tomato_Spider_mites_Two_spotted_spider_mite', 'Tomato__Target_Spot', 'Tomato__Tomato_mosaic_virus', 'Tomato__Tomato_YellowLeaf__Curl_Virus', 'Tomato_Late_blight']
+
+
+
+
+st.title("🌿 Plant Disease Classifier")
+st.write("Upload a plant leaf image and I'll predict the disease category.")
+
+
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    img = Image.open(uploaded_file)
+    st.image(img, caption='Uploaded Image', use_column_width=True)
+
+   
+    img = img.resize((150, 150))  # Resize as per model input
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0) / 255.0
+
+    
+    if st.button("Predict"):
+        prediction = model.predict(img_array)
+        class_index = np.argmax(prediction)
+        confidence = np.max(prediction)
+
+        st.success(f"Prediction: **{class_names[class_index]}**")
+        st.info(f"Confidence: {confidence*100:.2f}%")
+print(model.output_shape)
